e3bad2011039f0c9547f524dcb5736d6e1748b77
alexanderbol <alexbol99@gmail.com>
2023-09-14 15:20:45 +0300

    Save mail on unmount Compose component using useRef hook

--- src/components/EmailCompose.jsx
+++ src/components/EmailCompose.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {mailModelService} from "../services/mail-model.service.js";
 import {useLocation, useNavigate} from "react-router";
 import {useSearchParams} from "react-router-dom";
@@ -8,78 +8,104 @@ function EmailCompose(props) {
     const {pathname} = useLocation()
     const navigate = useNavigate()
     const [searchParams, _] = useSearchParams()
+    const mailRef = useRef(mail);
+
+    const id = searchParams.get("compose")
+
+    useEffect(() => {
+        // Update the mailRef whenever state changes
+        mailRef.current = mail;
+    }, [mail]);
 
     useEffect( () => {
-        async function createEmptyMail() {
-            try {
-                const newMail = await mailModelService.create()
-                if (newMail.id) setMail(newMail)
-            }
-            catch (err) {
-                console.log(err.message)
-                navigate(pathname)
-            }
+        if (id === "new") {
+            createEmptyMail()
+        } else {
+            fetchMailById(id)
         }
-        async function fetchMailById(id) {
-            try {
-                const existingMail = await mailModelService.getById(id)
-                setMail(existingMail)
-            }
-            catch (err) {
-                console.log(err.message)
-                navigate(pathname)
-            }
-        }
-        if (!mail) {
-            const id = searchParams.get("compose")
-            if (id === "new") {
-                createEmptyMail()
-            }
-            else {
-                fetchMailById(id)
-            }
-        }
-    },[])
 
-    async function onSubmitEmailCompose(event) {
-        event.preventDefault()
-        const form = event.currentTarget
-        if (form["To"].value.trim() === "")
-            return
-        const updatedMail = {
-            ...mail,
-            To: form["To"].value,
-            Subject: form["Subject"].value,
-            Body: form["Body"].value,
-            Date: new Date().toLocaleString(),
-            isDraft: false
+        return () => {
+            updateMail(mailRef.current)
         }
+    },[id])
+
+    async function createEmptyMail() {
         try {
-            await mailModelService.update(updatedMail)
+            const newMail = await mailModelService.create()
+            if (newMail.id) setMail(newMail)
+        }
+        catch (err) {
+            console.log(err.message)
+            navigate(pathname)
+        }
+    }
+    async function fetchMailById(id) {
+        try {
+            const existingMail = await mailModelService.getById(id)
+            setMail(existingMail)
+        }
+        catch (err) {
+            console.log(err.message)
+            navigate(pathname)
+        }
+    }
+
+    async function updateMail(mail) {
+        try {
+            await mailModelService.update(mail)
         }
         catch(err) {
             console.log(err.message)
+            navigate(pathname)
         }
+    }
+
+    function onChange(event) {
+        let { value, name: field } = event.target
+        setMail((prev) => ({ ...prev, [field]: value }))
+    }
+
+    async function onSubmit(event) {
+        event.preventDefault()
+        await updateMail({...mail, Date: new Date().toLocaleString(), isDraft: false})
         navigate(pathname)
     }
+
+    if (!mail) return "Reading mail"
 
     return (
         <div className="email-compose">
             <div className="compose-popup">
-                <h2 className="compose-popup-title">New Message</h2>
-                <form onSubmit={onSubmitEmailCompose}>
+                <h2 className="compose-popup-title">
+                    { id === "new" ? "New Message" : mail.Subject}
+                </h2>
+                <form onSubmit={onSubmit}>
                     <label htmlFor="To">To:</label>
-                    <input type="text" id="To" className="compose-input" placeholder="Recipient"
-                           defaultValue={mail?.To}/>
-                    <label htmlFor="CC">CC:</label>
-                    <input type="text" id="CC" className="compose-input" placeholder="CC"/>
-                    <label htmlFor="Subject">Subject:</label>
-                    <input type="text" id="Subject" className="compose-input" placeholder="Subject"
-                           defaultValue={mail?.Subject}
+                    <input type="text"
+                           id="To"
+                           name="To"
+                           className="compose-input"
+                           placeholder="Recipient"
+                           onChange={onChange}
+                           value={mail.To}
                     />
-                    <textarea id="Body" className="compose-textarea"
+                    {/*<label htmlFor="CC">CC:</label>*/}
+                    {/*<input type="text" id="CC" className="compose-input" placeholder="CC"/>*/}
+                    <label htmlFor="Subject">Subject:</label>
+                    <input type="text"
+                           id="Subject"
+                           name="Subject"
+                           className="compose-input"
+                           placeholder="Subject"
+                           onChange={onChange}
+                           value={mail.Subject}
+                    />
+                    <textarea id="Body"
+                              name="Body"
+                              className="compose-textarea"
                               placeholder="Compose your message..."
-                              defaultValue={mail?.Body}
+                              onChange={onChange}
+                              value={mail.Body}
                     ></textarea>
                     <button className="send-button">Send</button>
                 </form>
